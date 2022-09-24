ead9f22749a1b59eccf2b24334bdcb1492cb7736
malyushki <malyushki@gmail.com>
2022-09-24 12:22:42 +0300

    The first commit. Basic futures completed.

---
+++ src/GuessANumber.java
@@ -0,0 +1,62 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessANumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random randomNumber = new Random();
+        System.out.println("WELCOME TO GUESS A NUMBER GAME");
+        System.out.println("You can choose difficulty EASY, MEDIUM, HARD");
+        String difficulty = scanner.nextLine().toLowerCase(Locale.ROOT);
+        int n = 0;
+        while (n == 0) {
+            switch (difficulty) {
+                case "easy":
+                    n = 11;
+                    break;
+                case "medium":
+                    n = 51;
+                    break;
+                case "hard":
+                    n = 101;
+                    break;
+                default:
+                    System.out.println("You should choose from: EASY, MEDIUM and HARD to start the game");
+                    difficulty = scanner.nextLine().toLowerCase(Locale.ROOT);
+
+            }
+
+        }
+        System.out.println("Good Job!");
+        System.out.printf("Now you need to guess a number between 1 an %d.%n", (n - 1));
+        int computerNumber = randomNumber.nextInt(n);
+        while (true) {
+            System.out.printf("Guess a number (0-%d): ", n - 1);
+            String playerInput = scanner.nextLine();
+            int playerNumber;
+            boolean isValid = true;
+            for (int i = 0; i < playerInput.length(); i++) {
+                if (playerInput.charAt(i) < 48 || playerInput.charAt(i) > 57) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) {
+                playerNumber = Integer.parseInt(playerInput);
+                if (playerNumber == computerNumber) {
+                    System.out.println("You guessed it!");
+                    break;
+                } else if (playerNumber > computerNumber) {
+                    RandomMassage.tooHigh();
+                } else {
+                    RandomMassage.tooLow();
+                }
+            } else {
+                System.out.println("Invalid input!");
+            }
+        }
+    }
+}
---
+++ src/RandomMassage.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class RandomMassage {
+
+    static void tooLow() {
+        Random randomNumber = new Random();
+        int randomMassageNumber = randomNumber.nextInt(5);
+        String[] tooLow = {"Too Low.", "You need a bigger number", "If I was you I would pick a bigger number",
+                "Number you pick is too damn low", "Go bigger or go home"};
+        System.out.println(tooLow[randomMassageNumber]);
+    }
+    static void tooHigh() {
+        Random randomNumber = new Random();
+        int randomMassageNumber = randomNumber.nextInt(5);
+        String[] tooLow = {"Too High.", "You need a smaller number", "If I was you I would pick a smaller number",
+                "Number you pick is too damn high", "Go smaller or go home"};
+        System.out.println(tooLow[randomMassageNumber]);
+    }
+
+
+    }
+
+
